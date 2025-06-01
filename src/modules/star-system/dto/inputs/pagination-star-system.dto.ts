fed4a4b89ba361b260f53bb04fe9d71756741238
guilhermeramos31 <guilhermeramos31.gr@gmail.com>
2025-06-01 18:48:21 -0300

    refactor: extract pagination logic to base class

--- src/modules/star-system/dto/inputs/pagination-star-system.dto.ts
+++ src/modules/star-system/dto/inputs/pagination-star-system.dto.ts
@@ -1,22 +1,3 @@
-import { IsIn, IsInt, IsOptional, Min } from 'class-validator';
-import { Type } from 'class-transformer';
+import { Pagination } from '@shared/interceptors';
 
-export type OrderBy = 'asc' | 'desc';
-
-export class StarSystemPagination {
-  @IsOptional()
-  @Type(() => Number)
-  @IsInt()
-  @Min(1)
-  page: number = 1;
-
-  @IsOptional()
-  @Type(() => Number)
-  @IsInt()
-  @Min(1)
-  limit: number = 10;
-
-  @IsOptional()
-  @IsIn(['asc', 'desc'])
-  orderBy: OrderBy = 'asc';
-}
+export class StarSystemPagination extends Pagination {}
--- src/shared/interceptors/pagination.interceptor.ts
+++ src/shared/interceptors/pagination.interceptor.ts
@@ -7,6 +7,28 @@ import {
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Response } from 'express';
+import { IsIn, IsInt, IsOptional, Min } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export type OrderBy = 'asc' | 'desc';
+
+export class Pagination {
+  @IsInt()
+  @Min(1)
+  @IsOptional()
+  @Type(() => Number)
+  page: number = 1;
+
+  @IsInt()
+  @IsOptional()
+  @Min(1)
+  @Type(() => Number)
+  limit: number = 10;
+
+  @IsOptional()
+  @IsIn(['asc', 'desc'])
+  orderBy: OrderBy = 'asc';
+}
 
 interface PaginatedResponse<T> {
   data: T;
